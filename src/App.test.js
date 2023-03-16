e7d9ee3c1a852e440ad75ba204eed51076b1bab6
yair <yair@yedidia.net>
2023-03-16 23:44:52 +0200

    change

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,3 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test('fail', () => {
-  expect(false).toBe(true);
-});
